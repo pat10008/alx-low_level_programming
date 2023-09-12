541188b8cf333f63eb5adb245ca2f3db068d3d6a
pat10008 <patrobahabongo@gmail.com>
2023-09-11 19:18:10 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,4 +27,4 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
 	}	return (0);
-}
+}	
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Start point
+ * Description: 'Prints alphabet'
+ * Return: always return 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while(n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
