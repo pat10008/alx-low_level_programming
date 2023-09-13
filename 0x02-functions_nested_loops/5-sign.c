4c16badb81c6ed365d211b20a546b5c8854795cf
pat10008 <patrobahabongo@gmail.com>
2023-09-12 23:42:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: character check
+ * Return: 1 for aphabetic character or 0 for any
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
+
+
