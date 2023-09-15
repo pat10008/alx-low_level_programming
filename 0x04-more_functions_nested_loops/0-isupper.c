7452b15c8df4eca82a4475260072f5443f04a4ea
pat10008 <patrobahabongo@gmail.com>
2023-09-15 10:53:50 -0700

    Marvel

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,13 +7,14 @@
  * Return (1) for true (0) for false
  *
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-        if (c >='A') && c <= 'Z' )
+        if (c >='A' && c <= 'Z')
         {
                 return (1);
         }
         else
 	{
 		return (0);
-	}	
+	}
+}
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 
 /**
  * mul- check the code
- *@x: the number to be checked
- *@y: the number to be multiply
+ *@a: the number to be checked
+ *@b: the number to be multiply
  * Return: Always 0.
  */
 int mul(int a, int b)
