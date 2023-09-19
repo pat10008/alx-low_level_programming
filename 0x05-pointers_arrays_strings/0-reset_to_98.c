494e1db09e8e390688b7154275f419ce9b5bd43d
pat10008 <patrobahabongo@gmail.com>
2023-09-19 11:50:48 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - a function that resets the value of an int as parameter
  * the value of an int to 98
  * @n: input
- * Return: n
+ * Return: Always 0
  */
 void reset_to_98(int *n)
 {
